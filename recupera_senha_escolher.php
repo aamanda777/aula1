c059745cb67a5e9632b374105806aa43105c1e82
amanda <125467309+aamanda777@users.noreply.github.com>
2023-03-24 11:07:33 -0300

    documentos

--- recupera_senha_escolher.php
+++ recupera_senha_escolher.php
@@ -1,6 +1,6 @@
 <?php
-require('twig_carregar.php');
-require('pdo.inc.php');
+require 'twig_carregar.php';
+require 'pdo.inc.php';
 
 $token = $_GET['token'] ?? $_POST['token'] ?? false;
 
@@ -9,30 +9,33 @@ if (!$token) {
     die;
 }
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $password = $_POST['senha'] ?? false;
+    $password_confi = $_POST['senha_confirma'] ?? false;
     //trim = tirar os espaços no início e fim do texto
     $password = trim($password);
+    $password_confi = trim($password_confi);
     //tratamento para o caso de senha falsa
     //
+    if ($password == $password_confi) {
+        $sql = $pdo->prepare('UPDATE usuario SET senha = :senha , recupera_token = NULL WHERE recupera_token = :token');
+        $sql->execute([
+            ':senha' => password_hash($password, PASSWORD_BCRYPT),
+            ':token' => $token,
+        ]);
+        header('location:login.php?erro=3');
 
-    $sql = $pdo->prepare('UPDATE usuario SET senha = :senha , recupera_token = NULL WHERE recupera_token = :token');
-    $sql->execute([
-        ':senha' => password_hash($password, PASSWORD_BCRYPT),
-        ':token' => $token,
-    ]);
+        die;
+    }
 
-    header('location:login.php?erro=3');
-
-    die;
+    $msg = 'conseguiu errar a senha, né? 😂';
 }
-
 $sql = $pdo->prepare('SELECT * FROM usuario WHERE recupera_token = ?');
 $sql->execute([$token]);
 
 if ($sql->rowCount() == 1) {
-    echo $twig->render('recupera_senha_escolher.html' , ['token' => $token]);
-}else {
+    echo $twig->render('recupera_senha_escolher.html', ['token' => $token , 'msg' => $msg ?? false]);
+} else {
     header('location:login.php');
     die;
 }
